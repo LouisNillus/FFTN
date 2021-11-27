f23277d917499d81fbe3e6c35a0e9634cc0a056e
simon-gonand <simongonand@gmail.com>
2021-11-27 04:39:50 +0100

    idle, move forward + backward animations

--- Fight For The Night/Assets/Scripts/PlayerController.cs
+++ Fight For The Night/Assets/Scripts/PlayerController.cs
@@ -9,16 +9,25 @@ public class PlayerController : MonoBehaviour
 
     public Transform self;
     public Rigidbody selfRigidbody;
+    public Animator animator;
+
+    private Quaternion originalRotation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalRotation = self.rotation;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
+        if (x > -0.1 && x < 0.1)
+            animator.applyRootMotion = true;
+        else
+            animator.applyRootMotion = false;
         selfRigidbody.velocity = self.forward * x * speed * Time.deltaTime;
+        animator.SetFloat("Speed", x * speed * Time.deltaTime);
+        self.rotation = originalRotation;
     }
 }
