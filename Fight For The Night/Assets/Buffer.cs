e08d0dc4132c5bab9d32f3d93345588a886a922f
LouisNillus <louis.nillus@gmail.com>
2021-11-27 04:46:01 +0100

    Let's go dodo

--- Fight For The Night/Assets/Buffer.cs
+++ Fight For The Night/Assets/Buffer.cs
@@ -1,21 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Buffer : MonoBehaviour
 {
     public List<ComboInput> queue = new List<ComboInput>();
 
+    public ComboList allCombos;
+
+    public float clearCooldown;
+
+    public Coroutine clearDelay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(allCombos.combos[0].CompileToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D)) queue.Add(new ComboInput(KeyCode.D, TypeOfInput.Movement));
-        if (Input.GetKeyDown(KeyCode.G)) queue.Add(new ComboInput(KeyCode.G, TypeOfInput.Hit, 0.05f));
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ComboOverloadCheck();
+            if (clearDelay != null) StopCoroutine(clearDelay);
+            queue.Add(new ComboInput(KeyCode.D, TypeOfInput.Movement));
+            clearDelay = StartCoroutine(ClearDelay());
+
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ComboOverloadCheck();
+            if(clearDelay != null) StopCoroutine(clearDelay);
+            queue.Add(new ComboInput(KeyCode.G, TypeOfInput.Hit, 0.05f));
+            clearDelay = StartCoroutine(ClearDelay());
+
+        }
+    }
+
+    public IEnumerator ClearDelay()
+    {
+
+        float time = 0f;
+
+        while(time < clearCooldown)
+        {
+            time += Time.deltaTime;
+            yield return null;
+
+        }
+
+        queue.Clear();        
+    }
+
+    public void ComboOverloadCheck()
+    {
+        if (queue.Count > 4) queue.RemoveAt(0);
+    }
+
+    public Combo FindCombo(int length)
+    {
+        foreach(Combo c in allCombos.combos)
+        {
+            if (c.CompileToString().Length > 0 && CompileToString(length).Length > 0)
+                if (c.CompileToString() == CompileToString(length))
+                {
+                    Debug.Log(c.comboName);
+                    queue.Clear();
+                    return c;
+                }
+        }
+
+        return null;
+    }
+
+    public string CompileToString(int comboLength)
+    {
+
+        if (queue.Count < comboLength) return "";
+
+        string result = "";
+        int startIndex = queue.Count - comboLength;
+      
+
+        for (int i = startIndex; i < queue.Count; i++)
+        {
+            result += queue[i].key.ToString();
+        }
+
+        return result;
     }
 }
---
+++ Fight For The Night/Assets/Data/Combo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Combo
+{
+    [Header("         Combo Settings")]
+    public string comboName;
+    public List<ComboInput> inputs = new List<ComboInput>();
+    public string animationName;
+    public int damages;
+
+    public string CompileToString()
+    {
+        string result = "";
+
+        foreach(ComboInput ci in inputs)
+        {
+            result += ci.key.ToString();
+        }
+
+        return result;
+    }
+
+}
